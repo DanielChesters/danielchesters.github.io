09244f6fbbf8b5038f39010770e33a6dcd2548d5
Daniel Chesters <daniel.chesters@gmail.com>
2017-09-13 11:18:57 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,24 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
+if [[ $(git status -s) ]]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
+
+echo "Deleting old publication"
+rm -rf public
+git worktree prune
+rm -rf .git/worktrees/public/
+mkdir public
+
+echo "Checking out master branch into public"
+git worktree add -B master public origin/master
+
+echo "Removing existing files"
+rm -rf public/*
+
 # Build the project.
 hugo -t minimo
 
